528c2b82aaec8f26b739f92150fb5c2d5b11c9e8
yeonjae0311 <cyj9703@gmail.com>
2025-01-31 09:57:08 +0900

    프로젝트 생성

---
+++ GADI/src/main/java/com/basic/GADI/GadiApplication.java
@@ -0,0 +1,13 @@
+package com.basic.GADI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GadiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GadiApplication.class, args);
+	}
+
+}
