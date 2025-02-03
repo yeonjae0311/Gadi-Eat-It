ef7be2614080c3ef468efb294a715ae6a02bf6e3
BEAK SEJIN <ssjin119@gmail.com>
2025-02-03 13:20:59 +0900

    이메일 인증코드 발송

--- GADI/src/main/java/com/basic/GADI/controller/UserController.java
+++ GADI/src/main/java/com/basic/GADI/controller/UserController.java
@@ -3,6 +3,7 @@ package com.basic.GADI.controller;
 import com.basic.GADI.dto.request.RegisterRequestDto;
 import com.basic.GADI.dto.response.RegisterResponseDto;
 import com.basic.GADI.service.UserService;
+import jakarta.mail.MessagingException;
 import jakarta.validation.Valid;
 import com.basic.GADI.dto.request.LoginDto;
 import com.basic.GADI.dto.response.TokenResponseDto;
@@ -10,10 +11,7 @@ import com.basic.GADI.service.JwtService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/user")
@@ -25,13 +23,19 @@ public class UserController {
 
     @PostMapping("/login")
     public ResponseEntity<TokenResponseDto> login (@RequestBody LoginDto loginDto) {
+        String jwt = jwtService.createAccessToken(loginDto);
+        return ResponseEntity.status(HttpStatus.OK).header(jwt).build();
+    }
 
     @PostMapping("/register")
     public ResponseEntity<RegisterResponseDto> register(@RequestBody @Valid RegisterRequestDto registerRequestDto)  {
         RegisterResponseDto register = userService.register(registerRequestDto);
         return ResponseEntity.ok().body(register);
-        String jwt = jwtService.createAccessToken(loginDto);
+    }
 
-        return ResponseEntity.status(HttpStatus.OK).header(jwt).build();
+    @GetMapping("/email/auth/{email}")
+    public ResponseEntity<String> requestEmailAuth(@PathVariable("email") String email) throws MessagingException {
+        boolean isSend = userService.sendAuthMail(email);
+        return ResponseEntity.ok().body(isSend ? "인증번호가 전송되었습니다." : "인증번호 전송에 실패하였습니다.");
     }
 }
--- GADI/src/main/java/com/basic/GADI/service/UserService.java
+++ GADI/src/main/java/com/basic/GADI/service/UserService.java
@@ -5,20 +5,30 @@ import com.basic.GADI.dto.response.RegisterResponseDto;
 import com.basic.GADI.entity.User;
 import com.basic.GADI.exception.BusinessException;
 import com.basic.GADI.repository.UserRepository;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Random;
+
 @Service
+@RequiredArgsConstructor
 public class UserService {
 
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JavaMailSender javaMailSender;
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+    @Value("{spring.mail.username}")
+    private static String senderEmail;
 
 
     @Transactional
@@ -37,4 +47,52 @@ public class UserService {
 
         return new RegisterResponseDto(registerRequestDto.getUserEmail(), registerRequestDto.getUserName());
     }
+
+    public String createAuthCode() {
+        Random random = new Random();
+        StringBuilder key = new StringBuilder();
+
+        for (int i = 0; i < 6; i++) {
+            int number = random.nextInt(2);
+
+            switch (number) {
+                case 0: key.append((char) (random.nextInt(26) + 65));
+                case 1: key.append(random.nextInt(10));
+            }
+        }
+        return key.toString();
+    }
+
+    public MimeMessage createAuthMail(String mail, String authCode) throws MessagingException {
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+
+        mimeMessage.setFrom(senderEmail);
+        mimeMessage.setRecipients(MimeMessage.RecipientType.TO, mail);
+        mimeMessage.setSubject("[Gadi-Eat-It] 이메일 인증");
+        String body = "";
+        body += "<h3>안녕하세요. Gadi Eat IT! 입니다.</h3> ";
+        body += "<h3><span>요청하신 인증번호는 </span><span>" + authCode + "</span><span>입니다.</span></h3>";
+        body += "<h3>감사합니다.</h3>";
+        mimeMessage.setText(body, "UTF-8", "html");
+
+        return mimeMessage;
+    }
+
+
+    public boolean sendAuthMail(String sendEmail) throws MessagingException {
+        String authCode = createAuthCode();
+
+        MimeMessage createMail = createAuthMail(sendEmail, authCode);
+        try {
+            javaMailSender.send(createMail);
+            return true;
+        } catch (MailException e) {
+            return false;
+        }
+    }
+
+
+
+
+
 }
