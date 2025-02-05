73b96f53436ca3381310f05e83a1c10ae8ce23bc
BEAK SEJIN <ssjin119@gmail.com>
2025-02-05 17:09:06 +0900

    어노테이션 추가

--- GADI/src/main/java/com/basic/GADI/dto/request/PasswordLinkRequestDto.java
+++ GADI/src/main/java/com/basic/GADI/dto/request/PasswordLinkRequestDto.java
@@ -4,7 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+
+@Data
+@AllArgsConstructor
 @NoArgsConstructor
 public class PasswordLinkRequestDto {
+
     private String userEmail;
 }
