2309daa9898006bc5e66c6f45d44c4c8d18790a9
yeonjae0311 <cyj9703@gmail.com>
2025-02-07 09:43:09 +0900

    주석 추가

--- GADI/src/main/java/com/basic/GADI/repository/ResRepository.java
+++ GADI/src/main/java/com/basic/GADI/repository/ResRepository.java
@@ -17,20 +17,25 @@ public interface ResRepository extends JpaRepository<Restaurants, Long> {
     @Nonnull
     Optional<Restaurants> findById(@Nonnull Long resId);*/
 
+    // 레스토랑 단건 조회(별점 포함)
     @EntityGraph(attributePaths = {"ratings"})
     Optional<Restaurants> findByResDeleteAndResId(String resDelete, Long resId);
 
+    // 레스토랑 전체 리스트 조회 1 (레스토랑 전체 리스트)
     @Query("SELECT r FROM Restaurants r")
     Page<Restaurants> findAllRestaurants(Pageable pageable);
 
+    // 레스토랑 전체 리스트 조회 2 (레스토랑 아이디로 즐겨찾기, 별점 조회)
     @EntityGraph(attributePaths = {"favorites", "ratings"})
     List<Restaurants> findByResDeleteAndResIdIn(String resDelete, List<Long> restaurantIds);
 
   /*  @Query("SELECT r FROM Restaurants r JOIN r.favorites f WHERE f.user.userId = :userId")
     List<Restaurants> findFavoriteRestaurantsByUserId(@Param("userId") Long userId);*/
 
+    // 유저별 즐겨찾기 조회
     @EntityGraph(attributePaths = {"favorites"})
     Page<Restaurants> findByFavoritesUserUserId(Long userId, Pageable pageable);
 
+    // 레스토랑 단건 조회(레스토랑 entity만 포함)
     Optional<Restaurants> findByResId(Long resId);
 }
--- GADI/src/main/java/com/basic/GADI/repository/UserRepository.java
+++ GADI/src/main/java/com/basic/GADI/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.basic.GADI.repository;
 
 import com.basic.GADI.entity.User;
+import jakarta.annotation.Nonnull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,10 +11,14 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    // 유저 이메일 있는지 확인
     boolean existsByUserEmail(String userEmail);
 
+    // 유저이메일로 유저 단건 조회
     Optional<User> findByUserEmail(String userEmail);
 
+    // 유저 리스트 전체 조회
+    @Nonnull
     List<User> findAll();
 
 }
