00f4e664bb3baf9dee071b5edacb4d8fbd693c0f
BEAK SEJIN <ssjin119@gmail.com>
2025-02-25 15:15:43 +0900

    헤더 수정

--- VUE/src/router/index.js
+++ VUE/src/router/index.js
@@ -1,73 +1,87 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import MapView from '@/views/MapView.vue'
-import AboutView from '../views/AboutView.vue'
+import MapView from '@/views/MapView.vue' 
 import UpdateMyInfoView from '@/views/MyPage/UpdateMyInfoView.vue'
 import RegisterView from '@/views/RegisterView.vue'
 import AdminView from '@/views/AdminView.vue'
 import LoginView from '@/views/LoginView.vue'
 import AdminUserList from '@/components/AdminUserList.vue'
 import AdminResList from '@/components/AdminResList.vue'
-import AdminResDetail from '@/components/AdminResDetail.vue'
+import AdminResDetail from '@/components/AdminResDetail.vue' 
+import LayoutView from '@/views/layout/LayoutView.vue'
+import AboutView from '@/views/AboutView.vue'
+import MainView from '@/views/MainView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'about',
-      component: AboutView
+      path:'/',
+      name:'main',
+      component:MainView
     },
     {
-      path: '/login',
-      name: 'login',
-      component: LoginView
-    },
-    {
-      path: '/map',
-      name: 'map',
-      component: MapView
-    },
-    {
-      path: '/update',
-      name: 'update',
-      component: UpdateMyInfoView,
-      meta: { requiresLogin: true }
-    },
-    {
-      path: '/register',
-      name: 'register',
-      component: RegisterView
-    },
-    {
-      path: '/admin',
-      name: 'admin',
-      component: AdminView,
-      meta: { requiresLogin: true },
-      beforeEnter: (to, from, next) => {
-        const role = sessionStorage.getItem('role')
-        if (role === 'ADMIN') {
-          next() // 관리자면 통과
-        } else {
-          alert('접근 권한이 없습니다.')
-          next('/') // 홈으로 이동
-        }
-      },
+      path: '/gadi',
+      name: 'layout',
+      component: LayoutView,
       children: [
         {
-          path: 'user_list',
-          component: AdminUserList
+          path: '/',
+          name: 'about',
+          component: AboutView
         },
         {
-          path: 'res_list',
-          component: AdminResList
+          path: '/login',
+          name: 'login',
+          component: LoginView
         },
         {
-          path: 'res_list/:resId',
-          component: AdminResDetail,
-          props: true
+          path: '/map',
+          name: 'map',
+          component: MapView
+        },
+        {
+          path: '/update',
+          name: 'update',
+          component: UpdateMyInfoView,
+          meta: { requiresLogin: true }
+        },
+        {
+          path: '/register',
+          name: 'register',
+          component: RegisterView
+        },
+        {
+          path: '/admin',
+          name: 'admin',
+          component: AdminView,
+          meta: { requiresLogin: true },
+          beforeEnter: (to, from, next) => {
+            const role = sessionStorage.getItem('role')
+            if (role === 'ADMIN') {
+              next() // 관리자면 통과
+            } else {
+              alert('접근 권한이 없습니다.')
+              next('/') // 홈으로 이동
+            }
+          },
+          children: [
+            {
+              path: 'user_list',
+              component: AdminUserList
+            },
+            {
+              path: 'res_list',
+              component: AdminResList
+            },
+            {
+              path: 'res_list/:resId',
+              component: AdminResDetail,
+              props: true
+            }
+          ]
         }
       ]
-    }
+    } 
   ]
 })
 router.beforeEach((to, from, next) => {
---
+++ VUE/src/views/MainView.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <button @click="goRegisterView">회원가입</button>
+        <button @click="goMapView">지도보기</button>
+
+    </div>
+</template>
+
+<script setup>
+import router from '@/router';
+
+const goRegisterView = () => {
+    router.push('/register');
+}
+
+const goMapView = () => {
+    router.push('/map');
+}
+</script>
+
+<style scoped>
+
+button {
+    padding: 50px; 
+
+}
+
+
+</style>
